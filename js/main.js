634d20d13692973a93800de82ab61fc288b71ef3
Matheus <matheusalmeida016@gmail.com>
2023-05-23 18:34:23 -0400

    feat: site header and logo

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
+import { logo } from './icons.js'
+
 const root = document.querySelector('[data-js="root"]')
 
 root.innerHTML = `
-  <h1>Hello, world!</h1>
+  <div class="wrapper">
+    <header class="header">
+      <h1 class="header__logo" title="Pawxy Search">
+        <a href="/">
+          ${logo}
+
+          <span>Pawxy Search</span>
+        </a>
+      </h1>
+    </header>
+  </div>
 `
